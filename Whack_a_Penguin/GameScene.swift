0b9525d20546ba895b9563d366c49d2efc6356be
Owen Duignan <owenied@me.com>
2017-05-18 23:29:20 -0500

    Use SKAction sequences to let the player tap on a penguin to score, lose some points and then letting the game end after a period

--- Whack_a_Penguin/GameScene.swift
+++ Whack_a_Penguin/GameScene.swift
@@ -11,6 +11,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var numRounds = 0
     var popupTime = 0.85
     var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
@@ -46,6 +47,39 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>,
                                with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let tappedNodes = nodes(at: location)
+            
+            for node in tappedNodes {
+                if node.name == "charFriend" {
+                    
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.hit()
+                    score -= 5
+                    
+                    run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                    
+                } else if node.name == "charEnemy" {
+                    
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.charNode.xScale = 0.85
+                    whackSlot.charNode.yScale = 0.85
+                    
+                    whackSlot.hit()
+                    score += 1
+                    
+                    run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+                    
+                }
+            }
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -56,24 +90,40 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
-    popupTime *= 0.991
     
-    slots = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: slots) as! [WhackSlot]
-        slots[0].show(hideTime: popupTime)
+        numRounds += 1
         
-        if RandomInt(min: 0, max: 12) > 4 { slots[1].show(hideTime: popupTime) }
-        if RandomInt(min: 0, max: 12) > 8 { slots[2].show(hideTime: popupTime) }
-        if RandomInt(min: 0, max: 12) > 10 { slots[3].show(hideTime: popupTime) }
-        if RandomInt(min: 0, max: 12) > 11 { slots[4].show(hideTime: popupTime) }
-        
-        let minDelay = popupTime / 2
-        let maxDelay = popupTime * 2
-        let delay = RandomDouble(min: minDelay, max: maxDelay)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [unowned self] in
-            self.createEnemy()
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            return
         }
-    }
+        
+        popupTime *= 0.991
+    
+        slots = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: slots) as! [WhackSlot]
+            slots[0].show(hideTime: popupTime)
+        
+            if RandomInt(min: 0, max: 12) > 4 { slots[1].show(hideTime: popupTime) }
+            if RandomInt(min: 0, max: 12) > 8 { slots[2].show(hideTime: popupTime) }
+            if RandomInt(min: 0, max: 12) > 10 { slots[3].show(hideTime: popupTime) }
+            if RandomInt(min: 0, max: 12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+            let minDelay = popupTime / 2
+            let maxDelay = popupTime * 2
+            let delay = RandomDouble(min: minDelay, max: maxDelay)
+        
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [unowned self] in
+                self.createEnemy()
+            }
+        }
 }
 
 
