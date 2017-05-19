382af83be583ca9a906fae71e9b9c754ee9c027f
Owen Duignan <owenied@me.com>
2017-05-18 22:51:18 -0500

    Add functionality to display the penguin with SKAction moveBy()

--- Whack_a_Penguin/WhackSlot.swift
+++ Whack_a_Penguin/WhackSlot.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class WhackSlot: SKNode {
     
+    var isVisible = false
+    var isHit = false
     var charNode: SKSpriteNode!
     
     func configure(at position: CGPoint) {
@@ -30,6 +32,33 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if RandomInt(min: 0, max: 2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [unowned self] in
+            self.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 }
 
